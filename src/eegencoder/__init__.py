a3261a7b818fc4c0ceb0a1953a64ca2f59f17706
Vyom Mallick <vyommallick@gmail.com>
2025-11-29 17:31:00 +0530

    Add package documentation and metadata to init.py

--- src/eegencoder/__init__.py
+++ src/eegencoder/__init__.py
@@ -1,1 +1,12 @@
+"""
+EEGEncoder Package
+Motor Imagery EEG Classification using DSTS Architecture
+"""
 
+from .model import EEGEncoder, get_model
+from .data_loader import BCIC4_2A_Loader, get_dataloaders
+from .train import train_model, evaluate_model
+
+__version__ = "2.0.0"
+__author__ = "Chimera Org"
+__description__ = "EEGEncoder with Raspberry Pi optimization"
