808609d6280595cbdcece6e60ef30d80728ec3e1
Vyom Mallick <vyommallick@gmail.com>
2025-11-30 06:46:08 +0530

    Refactor imports in __init__.py for training functions

--- src/eegencoder/__init__.py
+++ src/eegencoder/__init__.py
@@ -5,7 +5,13 @@ Motor Imagery EEG Classification using DSTS Architecture
 
 from .model import EEGEncoder, get_model
 from .data_loader import BCIC4_2A_Loader, get_dataloaders
-from .train import train_model, evaluate_model
+from .train import (
+    train_model, 
+    train_epoch, 
+    evaluate, 
+    get_loso_dataloaders, 
+    run_loso_cross_validation
+)
 
 __version__ = "2.0.0"
 __author__ = "Chimera Org"
